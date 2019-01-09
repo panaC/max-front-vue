4413977f9ea8ba6e3a0ee1fe3d7227c066fb81db
pleroux <p.leroux@gmx.com>
2019-01-09 16:49:42 +0100

    add element locale

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,7 @@
 import Vue from "vue";
-import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
+import "./plugins/element";
 import HelloComponent from "./components/Hello.vue";
 
-Vue.use(ElementUI);
 
 let v = new Vue({
     el: "#app",
