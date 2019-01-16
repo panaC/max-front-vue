bc70898aee06f1f5bd6dcbebacaa01fcaba09b89
pleroux <p.leroux@gmx.com>
2019-01-16 07:29:26 +0100

    add navbar and center main content

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,18 +1,23 @@
 <template>
   <div>
-    <selectJourney
-      v-on:searchJourney="onDisplay"/>
-    <displayJourney v-bind:value='value'/>
+    <navbar />
+    <div class='content'>
+      <selectJourney
+        v-on:searchJourney="onDisplay"/>
+      <displayJourney v-bind:value='value'/>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Prop, Vue } from 'vue-property-decorator';
 import SelectJourney from "./select-journey.vue";
-import DisplayJourney, { Ivalue } from "./display-journey.vue"
+import DisplayJourney, { Ivalue } from "./display-journey.vue";
+import Navbar from "./navbar.vue";
 
 @Component({
   components: {
+    Navbar,
     SelectJourney,
     DisplayJourney,
   }
@@ -31,3 +36,13 @@ export default class App extends Vue {
   }
 }
 </script>
+
+<style scoped>
+@media (min-width: 1000px) {
+  .content {
+    width: 80%;
+    padding-top: 40px;
+    margin: auto;
+  }
+}
+</style>
